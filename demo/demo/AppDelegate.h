9239f9e6e490c510702ddea800b22c06c631d7e4
fanzhou <fanzhou@King.local>
2017-03-21 17:34:30 +0800

    运行demo

---
+++ demo/demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo
+//
+//  Created by fanzhou on 2017/3/21.
+//  Copyright © 2017年 fanzhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demo/demo/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  demo
+//
+//  Created by fanzhou on 2017/3/21.
+//  Copyright © 2017年 fanzhou. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "UIColor+Extension.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor randomColor];
+}
+
+
+@end
